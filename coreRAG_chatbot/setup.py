ea24796af80061f138a636bfdbde4df7b2c205c5
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-11 12:21:37 +0700

    Update loaddata and setup trials chatbot

---
+++ coreRAG_chatbot/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ChatbotMedical',
+    version='0.1',
+    author='Theanh13012004',
+    author_email='theanhtran13012004@gmail.com',
+    description='A chatbot for medical news',
+    packages=find_packages(),
+    install_requires=[
+    ],
+
+)
