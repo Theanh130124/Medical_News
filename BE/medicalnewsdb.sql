5b2e09a000c1ee574afd828605a37f0ad4ecf258
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-13 22:07:00 +0700

    not check yet issue

--- BE/medicalnewsdb.sql
+++ BE/medicalnewsdb.sql
@@ -54,7 +54,7 @@ CREATE TABLE Follow (
     FOREIGN KEY (follower_id) REFERENCES User(id) ON DELETE CASCADE,
     FOREIGN KEY (following_id) REFERENCES User(id) ON DELETE CASCADE
 );
-CREATE TABLE friends (
+CREATE TABLE Friends (
     first_user_id CHAR(36) NOT NULL,
     second_user_id CHAR(36) NOT NULL,
     status ENUM('PENDING', 'ACCEPTED', 'REJECTED') DEFAULT 'PENDING',
@@ -83,12 +83,19 @@ CREATE TABLE Post (
     user_id CHAR(36) NOT NULL,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    image VARCHAR(255),
     allow_comments BOOLEAN DEFAULT TRUE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
 );
+
+Create table ImagePost (
+	id CHAR(36) PRIMARY KEY DEFAULT (UUID()), -- UUID
+    post_image_url VARCHAR(255) NOT NULL ,
+    post_id char(36) NOT NULL , 
+    FOREIGN KEY (post_id) REFERENCES Post(id) on delete cascade
+);
+
 CREATE TABLE Comment (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()), -- UUID
     post_id CHAR(36) NOT NULL,
@@ -125,28 +132,27 @@ CREATE TABLE Search_history (
     keyword VARCHAR(255) NOT NULL,
     searched_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
-)
+);
 
 -- giới hạn lại việc lưu tìm kiếm 
-delimiter // 
+DELIMITER //
 
-create trigger limit_search_history
-after insert on Search_history
-for each row
-begin	
-	delete from Search_history
-    where user_id = NEW.user_id
-		and id not in (
-			select id from (
-				select id 
-                from Search_history
-                where user_id = NEW.user_id
-                order by search_at desc
-                limit 20
-                ) AS sub
-                );
-                END;
-                
+CREATE TRIGGER limit_search_history
+AFTER INSERT ON Search_history
+FOR EACH ROW
+BEGIN
+    DELETE FROM Search_history
+    WHERE user_id = NEW.user_id
+    AND id NOT IN (
+        SELECT id FROM (
+            SELECT id
+            FROM Search_history
+            WHERE user_id = NEW.user_id
+            ORDER BY searched_at DESC
+            LIMIT 20
+        ) AS temp
+    );
+END;
 //
 DELIMITER ;
                 
@@ -155,6 +161,67 @@ DELIMITER ;
 -- build B-tree -> giúp tìm kiếm theo trường user_id đã có index sẳn , mà không cần quét các trường còn lại (username , firstname ... )
 alter table Search_history 
 ADD INDEX idx_user_search (user_id, searched_at DESC);  
+
+
+
+-- insert data
+
+
+INSERT INTO Role (name, description) VALUES 
+('ADMIN', 'Administrator role'),
+('DOCTOR', 'Doctor role'),
+('USER', 'Normal user role');
+
+
+INSERT INTO Permission (name, description) VALUES 
+('CREATE_POST', 'Create new posts'),
+('EDIT_POST', 'Edit existing posts'),
+('DELETE_POST', 'Delete posts'); 
+
+INSERT INTO Role_Permission (role_id, permission_id)
+SELECT r.id, p.id
+FROM Role r JOIN Permission p
+WHERE r.name = 'ADMIN'; -- ADMIN có toàn quyền
+
+INSERT INTO Role_Permission (role_id, permission_id)
+SELECT r.id, p.id
+FROM Role r JOIN Permission p
+WHERE r.name = 'DOCTOR' AND p.name IN ('CREATE_POST');
+
+
+
+INSERT INTO User (role_id, username, first_name, last_name, phone_number, address, gender, date_of_birth, email, password, avatar)
+VALUES 
+((SELECT id FROM Role WHERE name = 'ADMIN'), 'admin', 'Admin', '1', '0933033801', '123 Admin St', 'MALE', '1990-01-01', 'admin123@gmail.com', 'hashedpassword', 'avatar1.png'),
+((SELECT id FROM Role WHERE name = 'DOCTOR'), 'theanh', 'Trần', 'Thế Anh', '0911328970', '420 Lê Văn Thọ , Phường 16 , Quận Gò Vấp , TPHCM', 'MALE', '2004-01-03', 'doctor123@gmail.com', 'hashedpassword', 'avatar2.png'),
+((SELECT id FROM Role WHERE name = 'USER'), 'minhtuyet', 'Nguyễn', 'Thị Minh Tuyết', '0522194804', '768/11 Lê Văn Lương , Xã Nhơn Đức , Huyện Nhà Bè , TPHCM', 'FEMALE', '2000-10-10', 'user123@gmail.com', 'hashedpassword', 'avatar3.png');
+
+
+
+INSERT INTO Post (user_id, title, content) 
+SELECT id, 'Bài Viết Đầu Tiên', 'Đây là nội dung của bài viết đầu tiên'
+FROM User WHERE username = 'admin';
+
+
+INSERT INTO Comment (post_id, user_id, content) 
+SELECT p.id, u.id, 'Đây là bình luận đầu tiên.'
+FROM Post p
+JOIN User u ON u.username = 'minhtuyet'
+WHERE p.title = 'Bài Viết Đầu Tiên';
+
+
+
+INSERT INTO Reaction (user_id, post_id, type)
+SELECT u.id, p.id, 'LIKE'
+FROM User u
+JOIN Post p
+WHERE u.username = 'minhtuyet' AND p.title = 'Bài Viết Đầu Tiên';
+
+
+INSERT INTO ImagePost (post_image_url, post_id)
+SELECT 'post1_img1.png', p.id FROM Post p WHERE p.title = 'Bài Viết Đầu Tiên';
+INSERT INTO ImagePost (post_image_url, post_id)
+SELECT 'post1_img2.png', p.id FROM Post p WHERE p.title = 'Bài Viết Đầu Tiên';
 
 
 
