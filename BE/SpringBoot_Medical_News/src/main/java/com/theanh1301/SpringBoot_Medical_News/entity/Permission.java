de535b03d352c90dbe90860eb52dd863ad90a0b9
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-14 22:42:46 +0700

    Update clean entity

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/entity/Permission.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/entity/Permission.java
@@ -6,26 +6,28 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.ColumnDefault;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Permission {
     @Id
     @Size(max = 36)
     @ColumnDefault("(uuid())")
     @Column(name = "id", nullable = false, length = 36)
-    private String id;
+    String id;
 
     @Size(max = 50)
     @NotNull
     @Column(name = "name", nullable = false, length = 50)
-    private String name;
+    String name;
 
     @Size(max = 255)
     @Column(name = "description")
-    private String description;
+    String description;
 
 }
--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/entity/SurveyOption.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/entity/SurveyOption.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -14,27 +15,29 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Table(name = "Survey_Option")
+
 public class SurveyOption {
     @Id
     @Size(max = 36)
     @ColumnDefault("(uuid())")
     @Column(name = "id", nullable = false, length = 36)
-    private String id;
+    String id;
 
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "post_id", nullable = false)
-    private Post post;
+    Post post;
 
     @Size(max = 255)
     @NotNull
     @Column(name = "option_text", nullable = false)
-    private String optionText;
+    String optionText;
 
     @ColumnDefault("CURRENT_TIMESTAMP")
     @Column(name = "created_at")
-    private Instant createdAt;
+    Instant createdAt;
 
 }
