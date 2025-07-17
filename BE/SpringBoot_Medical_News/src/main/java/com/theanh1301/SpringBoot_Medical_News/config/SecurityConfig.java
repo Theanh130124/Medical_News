9b3a68f921d99c35a9ca382b38e0d2bf3666b29a
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-17 10:52:20 +0700

    Update SpringSecurity and CustomAnnation for Validate DOB

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/config/SecurityConfig.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.theanh1301.SpringBoot_Medical_News.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +12,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -21,9 +27,13 @@ import javax.crypto.spec.SecretKeySpec;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity // phần quyền theo @PreAuthorize
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal=true)
 public class SecurityConfig {
 
-    private final String[] PUBLIC_ENDPOINTS = {
+
+    //Field final được gán thi Spring không injection
+    final String[] PUBLIC_ENDPOINTS = {
             "/api/users",
             "/api/auth/login",
             "/api/auth/logout",
@@ -32,11 +42,12 @@ public class SecurityConfig {
 
 
     @Value("${jwt.signerKey}")
-    private String jwtSignerKey;
+    @NonFinal
+    String jwtSignerKey;
 
 
-    @Autowired
-    private CustomJ
+
+    CustomJwtDecoder customJwtDecoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -56,10 +67,24 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
+
+
     @Bean
     public JwtDecoder jwtDecoder() {
         SecretKeySpec secretKey = new SecretKeySpec(jwtSignerKey.getBytes(), "HS512");
         return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS512).build();
+    }
+
+
+
+    //Custom lại tiền tố ->//Trong scope thì vẫn còn ROLE_ADMIN , CREATE_POST ...  , trong grantedAuthority -> không có tiền tố ROLE_
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(""); // gán tiền tố -> đã gán bên AuthenticationService
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 
 
---
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/service/UserService.java
@@ -0,0 +1,19 @@
+package com.theanh1301.SpringBoot_Medical_News.service;
+
+
+import com.theanh1301.SpringBoot_Medical_News.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserService {
+
+    UserRepository userRepository;
+
+}
