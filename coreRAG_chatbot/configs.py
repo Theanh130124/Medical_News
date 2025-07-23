9ac665b1f5a0be5160660ee22d84ea4dc16a09c3
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-23 15:15:27 +0700

    Update done rerank

--- coreRAG_chatbot/configs.py
+++ coreRAG_chatbot/configs.py
@@ -6,6 +6,9 @@ load_dotenv()
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV", "us-east1-gcp")
+MODEL_LLM_NAME ="deepseek/deepseek-r1-distill-llama-70b:free"
+MODEL_EMBEDING_NAME ="dangvantuan/vietnamese-embedding"
+MODEL_CROSS_ENCODER_NAME = "itdainb/PhoRanker"
 INDEX_NAME = "medical-chatbot"
 DATA_FOLDER = "./DataChatbot/"
 TRAINED_LOG = "trained_files.log"  #Nếu đã từng train thì sẽ lưu tên file lại vào đây 
