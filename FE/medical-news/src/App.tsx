cc147fee672ccf1facec22b2959da79cd5a95efd
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-25 17:01:32 +0700

    Update react-router-dom , react bootstrap

--- FE/medical-news/src/App.tsx
+++ FE/medical-news/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './component/layout/Header';
+import Footer from './component/layout/Footer';
+import Home from './component/Home';
 
-function App() {
+
+const App = () => {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <BrowserRouter>
+      <Header/> 
+      <Container fluid>
+
+        <Routes>
+          <Route path='/' element={<Home/>} />
+
+        </Routes>
+
+
+      </Container>
+
+      <Footer/>
+
+
+
+    </BrowserRouter>
+
+
+  )
 }
 
 export default App;
