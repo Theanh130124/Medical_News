1aedf20605d19bb767854fce86dbb63cdbd74d7c
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-26 16:40:00 +0700

    Update send mail for  create doctor

---
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.theanh1301.SpringBoot_Medical_News.service;
+
+import com.theanh1301.SpringBoot_Medical_News.dto.response.UserResponse;
+import com.theanh1301.SpringBoot_Medical_News.entity.User;
+
+public interface EmailService {
+    void sendAccountDoctorInfoEmail(User user);
+}
