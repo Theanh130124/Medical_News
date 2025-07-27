140fb387b70d0e857795561d3861f671c5bcb20d
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-27 14:20:06 +0700

    Update API POST

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/controller/ApiPostController.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/controller/ApiPostController.java
@@ -1,4 +1,33 @@
 package com.theanh1301.SpringBoot_Medical_News.controller;
 
+
+import com.theanh1301.SpringBoot_Medical_News.dto.request.PostCreationRequest;
+import com.theanh1301.SpringBoot_Medical_News.dto.response.ApiResponse;
+import com.theanh1301.SpringBoot_Medical_News.dto.response.PostResponse;
+import com.theanh1301.SpringBoot_Medical_News.service.PostService;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+@FieldDefaults(level= AccessLevel.PRIVATE , makeFinal=true)
 public class ApiPostController {
+
+
+    PostService postService;
+
+
+    @PostMapping
+    public ApiResponse<PostResponse> createPost(@ModelAttribute @Valid PostCreationRequest request){
+        var res = postService.createPost(request);
+        return ApiResponse.<PostResponse>builder().result(res).message("Tạo bài viết thành công").build();
+    }
+
 }
