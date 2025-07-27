71301d0a829db5bf30611b5ca6e3c5c01b110371
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-27 21:43:36 +0700

    Update delete post perrmison dont test

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/PostRepository.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/PostRepository.java
@@ -4,4 +4,5 @@ import com.theanh1301.SpringBoot_Medical_News.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, String> {
+
 }
