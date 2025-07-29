41c4ff301ec4348187c264b346b42cb08f6d4d12
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-29 12:30:09 +0700

    Update count response

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/ReactionRepository.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/ReactionRepository.java
@@ -4,9 +4,13 @@ import com.theanh1301.SpringBoot_Medical_News.dto.response.ReactionResponse;
 import com.theanh1301.SpringBoot_Medical_News.entity.Post;
 import com.theanh1301.SpringBoot_Medical_News.entity.Reaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReactionRepository extends JpaRepository<Reaction,String> {
     List<Reaction> getReactionByPost(Post post);
+    @Query("SELECT COUNT(r) FROM Reaction r where r.post = :post")
+    long countReactionByPost(@Param("post") Post post); //:post = "post"
 }
