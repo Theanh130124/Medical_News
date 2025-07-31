c49fd6ff862e1fd087b8a86b35d40f814ed8cf09
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-31 20:52:03 +0700

    UPDATE API follow dont test

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/mapper/FollowMapper.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/mapper/FollowMapper.java
@@ -5,12 +5,16 @@ import com.theanh1301.SpringBoot_Medical_News.dto.request.FollowRequest;
 import com.theanh1301.SpringBoot_Medical_News.dto.response.FollowResponse;
 import com.theanh1301.SpringBoot_Medical_News.entity.Follow;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring" ,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface FollowMapper {
 
+    //id -> chính là entity FollowId
+    @Mapping(target = "followerId",source = "follower")
+    @Mapping(target = "followingId",source = "following")
     FollowResponse toFollowResponse(Follow follow);
 
     Follow toFollow(FollowRequest request);
