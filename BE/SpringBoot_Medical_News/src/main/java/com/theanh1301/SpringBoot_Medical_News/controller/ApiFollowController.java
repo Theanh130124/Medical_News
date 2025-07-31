c10005e1d12f4cae9a1174f3db69df5628e2c116
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-07-31 20:52:03 +0700

    UPDATE API follow dont test

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/controller/ApiFollowController.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/controller/ApiFollowController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +21,12 @@ public class ApiFollowController {
 
     FollowService followService;
 
+
+    //Xem permission
+
+
+    //Chỉ có followerId đúng mới được làm
+//    @PostAuthorize("returnObject.result.followingId.")
     @PostMapping
     public ApiResponse<FollowResponse> follow(@RequestBody FollowRequest request) {
         var res = followService.follow(request);
@@ -27,15 +34,33 @@ public class ApiFollowController {
 
     }
 
+    //Chỉ có followerId đúng mới được làm
     @DeleteMapping
     public ApiResponse<Void> unfollow(@RequestBody FollowRequest request){
         followService.unfollow(request);
         return ApiResponse.<Void>builder().message("Đã bỏ theo dõi thành công").build();
     }
 
-    @GetMapping("/follwers/{userId}")
+    //Chỉ có followerId đúng mới được làm
+    @GetMapping("/followers/{userId}")
     public ApiResponse<Page<FollowResponse>> getFollowers(@PathVariable String userId , Pageable pageable){
-        //Thêm count nữa
+        var res = followService.getFollowers(userId,pageable);
+        long count = followService.countFollowers(userId);
+
+        return ApiResponse.<Page<FollowResponse>>builder()
+                .result(res)
+                .count(count)
+                .message("Lấy danh sách số người đang theo dõi thành công").build();
+
+    }
+
+    //Chỉ có followerId đúng mới được làm
+    @GetMapping("/following/{userId}")
+    public ApiResponse<Page<FollowResponse>> getFollowing(@PathVariable String userId , Pageable pageable){
+        var res = followService.getFollowings(userId,pageable);
+        long count = followService.countFollowing(userId);
+        return ApiResponse.<Page<FollowResponse>>builder()
+                .result(res).count(count).message("Lấy danh sách người bạn đang theo dõi thành công").build();
 
     }
 
--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/FollowRepository.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/FollowRepository.java
@@ -6,8 +6,10 @@ import com.theanh1301.SpringBoot_Medical_News.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow,String> {
@@ -19,4 +21,11 @@ public interface FollowRepository extends JpaRepository<Follow,String> {
     Page<Follow> findAllByFollowingId(User following, Pageable pageable); //Đang theo dõi
 
     Optional<Follow> findById(FollowId followId);
+
+    //Số người đang theo dõi mình
+    @Query("select count(f) From Follow f where f.following = :user")
+    long countFollowers(@Param("user")User user);
+
+    @Query("select count(f) From Follow  f where f.follower = :user")
+    long countFollowing(@Param("user")User user);
 }
