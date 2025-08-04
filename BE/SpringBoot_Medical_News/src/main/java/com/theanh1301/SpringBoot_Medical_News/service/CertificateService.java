58a34f59ff5846993167ad494f8e27db70ca06d3
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-08-04 21:06:26 +0700

    Update load certification

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/service/CertificateService.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/service/CertificateService.java
@@ -1,9 +1,15 @@
 package com.theanh1301.SpringBoot_Medical_News.service;
 
 import com.theanh1301.SpringBoot_Medical_News.dto.request.CertificateCreationRequest;
+import com.theanh1301.SpringBoot_Medical_News.dto.request.CertificateUpdateRequest;
 import com.theanh1301.SpringBoot_Medical_News.dto.response.CertificateResponse;
+import com.theanh1301.SpringBoot_Medical_News.enums.CertificateStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 public interface CertificateService {
     CertificateResponse createCertificate(CertificateCreationRequest request);
-
+    CertificateResponse updateCertificate(String id , CertificateUpdateRequest request);
+    Page<CertificateResponse> getCertificatesByStatus(CertificateStatus status , Pageable pageable);
 }
