06c9b34508afdf4aa00c6988c373eadcbc65fc9c
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-08-04 21:06:26 +0700

    Update load certification

---
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/controller/CertificateController.java
@@ -0,0 +1,46 @@
+package com.theanh1301.SpringBoot_Medical_News.controller;
+
+
+import com.theanh1301.SpringBoot_Medical_News.config.PaginationProperties;
+import com.theanh1301.SpringBoot_Medical_News.dto.response.CertificateResponse;
+import com.theanh1301.SpringBoot_Medical_News.enums.CertificateStatus;
+import com.theanh1301.SpringBoot_Medical_News.service.CertificateService;
+import com.theanh1301.SpringBoot_Medical_News.utils.PaginationUtils;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE , makeFinal = true)
+@Controller
+public class CertificateController {
+
+
+    CertificateService certificateService;
+    PaginationProperties paginationProperties;
+
+
+
+    @GetMapping("/certificate")
+    public String formCertificate(Model model , @RequestParam(value = "status", required = false) CertificateStatus status, @RequestParam(required = false) Integer page,
+                                  @RequestParam(required = false)  Integer size )  {
+
+        Pageable pageable = PaginationUtils.createPageable(page,size,paginationProperties);
+
+        Page<CertificateResponse> certificatePage;
+
+        certificatePage = certificateService.getCertificatesByStatus(status, pageable);
+        model.addAttribute("certificate", certificatePage);
+        model.addAttribute("allStatuses", CertificateStatus.values());
+        model.addAttribute("selectedStatus", status);
+        return "certificate";
+    }
+}
--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/service/impl/CertificateServiceImpl.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/service/impl/CertificateServiceImpl.java
@@ -4,10 +4,12 @@ package com.theanh1301.SpringBoot_Medical_News.service.impl;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.theanh1301.SpringBoot_Medical_News.dto.request.CertificateCreationRequest;
+import com.theanh1301.SpringBoot_Medical_News.dto.request.CertificateUpdateRequest;
 import com.theanh1301.SpringBoot_Medical_News.dto.response.CertificateResponse;
 import com.theanh1301.SpringBoot_Medical_News.entity.Certificate;
 import com.theanh1301.SpringBoot_Medical_News.entity.Doctor;
 import com.theanh1301.SpringBoot_Medical_News.entity.User;
+import com.theanh1301.SpringBoot_Medical_News.enums.CertificateStatus;
 import com.theanh1301.SpringBoot_Medical_News.enums.RoleName;
 import com.theanh1301.SpringBoot_Medical_News.exception.AppException;
 import com.theanh1301.SpringBoot_Medical_News.exception.ErrorCode;
@@ -20,6 +22,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -41,21 +45,21 @@ public class CertificateServiceImpl implements CertificateService {
     Cloudinary cloudinary;
 
     @Override
-    public CertificateResponse createCertificate(CertificateCreationRequest request){
+    public CertificateResponse createCertificate(CertificateCreationRequest request) {
 
 
-        if(certificateRepository.existsCertificateByCertificateNumber(request.getCertificateNumber())){
+        if (certificateRepository.existsCertificateByCertificateNumber(request.getCertificateNumber())) {
             throw new AppException(ErrorCode.CERTIFICATE_NUMBER_EXISTS);
         }
 
         Certificate certificate = certificateMapper.toCertificate(request);
-        MultipartFile  imageCertificate = request.getImageCertificate();
+        MultipartFile imageCertificate = request.getImageCertificate();
         if (imageCertificate != null && !imageCertificate.isEmpty()) {
-            try{
+            try {
                 Map res = cloudinary.uploader().upload(imageCertificate.getBytes(),
                         ObjectUtils.asMap("resource_type", "auto"));
                 certificate.setImageCertificate(res.get("secure_url").toString());
-            }catch (IOException ex){
+            } catch (IOException ex) {
                 Logger.getLogger(CertificateServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
             }
 
@@ -66,7 +70,26 @@ public class CertificateServiceImpl implements CertificateService {
         certificate.setDoctor(doctor);
 
 
-
         return certificateMapper.toCertificateResponse(certificateRepository.save(certificate));
     }
+
+
+    @Override
+    public CertificateResponse updateCertificate(String id, CertificateUpdateRequest request) {
+        Certificate certificate = certificateRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.CERTIFICATE_NOT_FOUND));
+        certificateMapper.updateCertificate(certificate, request); // map cái status
+
+        return  certificateMapper.toCertificateResponse(certificateRepository.save(certificate));
+
+    }
+
+
+    @Override
+    public Page<CertificateResponse> getCertificatesByStatus(CertificateStatus status , Pageable pageable) {
+        return certificateRepository.getCertificateByStatus(status, pageable).map(certificateMapper::toCertificateResponse);
+
+    }
 }
+
+
+
