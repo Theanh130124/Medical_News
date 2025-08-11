2299ab792dbfa70440be5c8e1ed7c5c2b37c767a
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-08-11 17:15:26 +0700

    Update api friend

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/FriendRepository.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/repository/FriendRepository.java
@@ -1,7 +1,26 @@
 package com.theanh1301.SpringBoot_Medical_News.repository;
 
 import com.theanh1301.SpringBoot_Medical_News.entity.Friend;
+import com.theanh1301.SpringBoot_Medical_News.entity.FriendId;
+import com.theanh1301.SpringBoot_Medical_News.entity.User;
+import com.theanh1301.SpringBoot_Medical_News.enums.FriendStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FriendRepository extends JpaRepository<Friend,String> {
+
+    boolean existById(FriendId friendId);
+    Optional<Friend> findById(FriendId friendId);
+
+    Page<Friend> findAllByFirstUserAndStatus(User user, FriendStatus status, Pageable pageable);
+    Page<Friend> findAllBySecondUserAndStatus(User user, FriendStatus status, Pageable pageable);
+
+
+    long countByFirstUserAndStatus(User user, FriendStatus status);
+    long countBySecondUserAndStatus(User user, FriendStatus status);
+
+    void deleteById(FriendId id);
 }
---
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/service/FriendService.java
@@ -0,0 +1,19 @@
+package com.theanh1301.SpringBoot_Medical_News.service;
+
+import com.theanh1301.SpringBoot_Medical_News.dto.request.FriendCreationRequest;
+import com.theanh1301.SpringBoot_Medical_News.dto.request.FriendUpdateRequest;
+import com.theanh1301.SpringBoot_Medical_News.dto.response.FriendResponse;
+import com.theanh1301.SpringBoot_Medical_News.entity.Friend;
+import com.theanh1301.SpringBoot_Medical_News.repository.FriendRepository;
+import org.mapstruct.MappingTarget;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface FriendService {
+
+    FriendResponse sendRequest(FriendCreationRequest request);
+    FriendResponse updateRequest(FriendUpdateRequest request, String firstUserId, String secondUserId);
+    void deleteFriend(String firstUserId, String secondUserId);
+    Page<FriendResponse> getFriends(String userId, Pageable pageable);
+    Page<FriendResponse> getPendingRequests(String userId, Pageable pageable);
+}
