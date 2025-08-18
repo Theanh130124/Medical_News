eaf53134be701485fe7c8574c1afa96b821a062a
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-08-18 15:07:49 +0700

    Update register

---
+++ FE/medical-news/src/configs/Apis.tsx
@@ -0,0 +1,49 @@
+import axios from "axios"
+import cookie from 'react-cookies';
+
+
+
+const BASE_URL = "http://localhost:8080/SpringBoot_Medical_News/api/";
+export const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dxiawzgnz/image/upload";
+export const CLOUDINARY_PRESET = "healthapp";
+
+
+
+export const endpoint = {
+
+
+    'register': '/users',
+
+
+
+} 
+
+//json -> có token
+export const authApis = () => {
+    return axios.create({
+        baseURL: BASE_URL,
+        headers: {
+            'Authorization': `Bearer ${cookie.load('token')}`,
+            //Bổ sung để lưu from
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+//form -> có token
+export const authformdataApis = () => {
+    return axios.create({
+        baseURL: BASE_URL,
+        headers: {
+            'Authorization': `Bearer ${cookie.load('token')}`,
+            //Bổ sung để lưu from
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+}
+
+
+//không có token
+export default axios.create({
+    baseURL: BASE_URL
+})
