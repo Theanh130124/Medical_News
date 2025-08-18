88014fa8ce2519e72acbe6b923467dc139b65299
Trần Thế Anh <theanhtran13012004@gmail.com>
2025-08-18 23:52:23 +0700

    Update login fe

--- BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/exception/ErrorCode.java
+++ BE/SpringBoot_Medical_News/src/main/java/com/theanh1301/SpringBoot_Medical_News/exception/ErrorCode.java
@@ -15,7 +15,7 @@ public enum ErrorCode {
     USER_EXISTS(1001, "Tên tài khoản đã tồn tại",HttpStatus.BAD_REQUEST),
     USERNAME_INVALID(1003,"Tên tài khoản không được nhỏ hơn {min} ký tự",HttpStatus.BAD_REQUEST),
     PASSWORD_INVALID(1004,"Mật khẩu không được nhỏ hơn {min} ký tự",HttpStatus.BAD_REQUEST),
-    USER_NOT_EXISTS(1005,"User không tồn tại",HttpStatus.NOT_FOUND),
+    USER_NOT_EXISTS(1005,"Tài khoản không tồn tại",HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006,"Không đăng nhập thành công",HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1006,"Không có quyền thực hiện",HttpStatus.FORBIDDEN),
     INVALID_DOB(1007 ,"Bạn phải trên {min} tuổi ",HttpStatus.BAD_REQUEST),
--- FE/medical-news/src/App.tsx
+++ FE/medical-news/src/App.tsx
@@ -3,17 +3,28 @@ import { Container } from 'react-bootstrap';
 import Header from './component/layout/Header';
 import Footer from './component/layout/Footer';
 import Home from './component/Home';
+import cookie from 'react-cookies'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from './component/auth/Register';
 import Login from './component/auth/Login';
+import { useEffect, useReducer } from 'react';
+import MyUserReducer from './reducers/MyUserReducer';
+import { MyDipatcherContext, MyUserContext } from './configs/MyContexts';
 
 
 const App = () => {
 
+  //dispatch nhận action.type bên MyUserReducer.tsx-> F5 sẽ không mất vì đã lưu cookie
+
+  const [user, dispatch] = useReducer(MyUserReducer, cookie.load('user') || null);
+
+
+
 
 
   return (
-
+    <MyUserContext.Provider value={user}>
+       <MyDipatcherContext.Provider value={dispatch}>
     <BrowserRouter>
       <Header/> 
       <Container fluid>
@@ -33,7 +44,8 @@ const App = () => {
 
 
     </BrowserRouter>
-
+    </MyDipatcherContext.Provider>
+ </MyUserContext.Provider>
 
   )
 }
